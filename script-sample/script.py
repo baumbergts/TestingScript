f64043ff62be151d682fe8ca33c871e089bc057f
Sebastiao <fc46599@alunos.fc.ul.pt>
2020-11-27 13:09:44 +0000

    first commit

---
+++ script-sample/script.py
@@ -0,0 +1,16 @@
+import csv
+import sys
+import os 
+filename = os.environ.get("FILE_NAME")
+path = "input/" + filename
+with open(path) as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            print(f'Column names are {", ".join(row)}')
+            line_count += 1
+        else:
+            print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
+            line_count += 1
+    print(f'Processed {line_count} lines.')
